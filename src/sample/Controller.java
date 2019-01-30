7fb1aae9e5e6b94bafe49e1dbe9b00743c9d2907
BT_1N3_30 <jaer[oghi3jq5ry>
2019-01-30 09:56:50 -0500

    Beginner

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
+import javax.sound.midi.*;
 
-public class Controller {
+public class Controller{
+
+    public static void main(String[] args) {
+        try{
+            /* Create a new Sythesizer and open it. Most of
+             * the methods you will want to use to expand on this
+             * example can be found in the Java documentation here:
+             * https://docs.oracle.com/javase/7/docs/api/javax/sound/midi/Synthesizer.html
+             */
+            Synthesizer midiSynth = MidiSystem.getSynthesizer();
+            midiSynth.open();
+
+            //get and load default instrument and channel lists
+            Instrument[] instr = midiSynth.getDefaultSoundbank().getInstruments();
+            MidiChannel[] mChannels = midiSynth.getChannels();
+
+            midiSynth.loadInstrument(instr[0]);//load an instrument
+
+            int note = 45;
+
+            mChannels[0].noteOn(note, 300);//On channel 0, play note number 60 with velocity 100
+            try { Thread.sleep(1000); // wait time in milliseconds to control duration
+            } catch( InterruptedException e ) { }
+            mChannels[0].noteOff(note);//turn of the note
+
+
+        } catch (MidiUnavailableException e) {}
+    }
+
 }
