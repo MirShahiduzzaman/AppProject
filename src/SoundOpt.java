32498dccab6c9b18070f2d04731ac742fc35c29b
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2019-02-03 22:47:56 -0500

    Added Tulip Pic. Need to work on opening and writing a name for the file.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/SoundOpt.java
+++ src/SoundOpt.java
@@ -1,7 +1,21 @@
-import javafx.scene.image.Image;
 public class SoundOpt {
-    public SoundOpt(Picture picture)
+    public static void avgEach(String picture)
     {
+        System.out.println("pix");
+        Picture pix = new Picture(picture);
 
+        System.out.println("pix");
+        Picture smallP = pix.scale(.5,.5);
+        System.out.println("pix");
+
+        smallP.write("normal");
+        System.out.println("pix");
+
+        smallP.explore();
+    }
+
+    public static void main(String[] args)
+    {
+        avgEach("Tulips.jpg");
     }
 }
