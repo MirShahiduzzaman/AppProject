e97f3ee735631de6688775f017bfb3511219f7ba
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2019-02-05 19:49:52 -0500

    changed picture from Tulips to Motorcycle
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/Picture.java
+++ src/Picture.java
@@ -4,8 +4,57 @@ import java.util.*;
 public class Picture extends SimplePicture {
 
     ArrayList<Integer> notes = new ArrayList<Integer>();
+    ///////////////////// constructors //////////////////////////////////
 
-    public Picture(BufferedImage image) {
+    /**
+     * Constructor that takes no arguments
+     */
+    public Picture ()
+    {
+        /* not needed but use it to show students the implicit call to super()
+         * child constructors always call a parent constructor
+         */
+        super();
+    }
+
+    /**
+     * Constructor that takes a file name and creates the picture
+     * @param fileName the name of the file to create the picture from
+     */
+    public Picture(String fileName)
+    {
+        // let the parent class handle this fileName
+        super(fileName);
+    }
+
+    /**
+     * Constructor that takes the width and height
+     * @param height the height of the desired picture
+     * @param width the width of the desired picture
+     */
+    public Picture(int height, int width)
+    {
+        // let the parent class handle this width and height
+        super(width,height);
+    }
+
+    /**
+     * Constructor that takes a picture and creates a
+     * copy of that picture
+     * @param copyPicture the picture to copy
+     */
+    public Picture(Picture copyPicture)
+    {
+        // let the parent class do the copy
+        super(copyPicture);
+    }
+
+    /**
+     * Constructor that takes a buffered image
+     * @param image the buffered image to use
+     */
+    public Picture(BufferedImage image)
+    {
         super(image);
     }
 
