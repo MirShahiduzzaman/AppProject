3c2f23ebb5881b4d9f43a04fd1816b1e686e3173
BT_1N3_17 <35egav>
2019-02-07 09:24:47 -0500

    progress

--- src/sample/Picture.java
+++ src/sample/Picture.java
@@ -14,54 +14,27 @@ import java.util.ArrayList;
 public class Picture {
 
     private int rgbValue;
-    private ArrayList<pixel> col ;
-    private ArrayList<pixel> row ;
+    private ArrayList<Pixel> col ;
+    private ArrayList<Pixel> row ;
     int width;
     int height;
 
-    public Image picelate(Image Import,int boxCount){
+    public Picture(){
+        super();
+    }
 
-        PixelReader imgReader = new PixelReader() {
-            @Override
-            public PixelFormat getPixelFormat() {
-                return null;
-            }
-
-            @Override
-            public int getArgb(int x, int y) {
-                return 0;
-            }
-
-            @Override
-            public Color getColor(int x, int y) {
-                return null;
-            }
-
-            @Override
-            public <T extends Buffer> void getPixels(int x, int y, int w, int h, WritablePixelFormat<T> pixelformat, T buffer, int scanlineStride) {
-
-            }
-
-            @Override
-            public void getPixels(int x, int y, int w, int h, WritablePixelFormat<ByteBuffer> pixelformat, byte[] buffer, int offset, int scanlineStride) {
-
-            }
-
-            @Override
-            public void getPixels(int x, int y, int w, int h, WritablePixelFormat<IntBuffer> pixelformat, int[] buffer, int offset, int scanlineStride) {
-
-            }
-        }
+    public Image pixelate(Image Import,int boxCount){
 
         width = boxCount;
         height = boxCount;
 
-        col = new ArrayList<>(width);
-        row = new ArrayList<>(height);
+        Pixel[][] pixels = getPixels2D();
+
+        byte[] buffer = new byte[]{10,10,10};
+        int offset = 0;
 
         for(int i = 0; i<col.size();i++){
 
-            imgReader.getPixels();
 
             for(int j=0;j<row.size();j++){
 
